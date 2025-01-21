f6cd62a88f60e514ed4e849aa0e01ac0cf5a223d
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2025-01-21 09:16:09 +0100

    feat: add items to check data files for in template folder

--- _template-folder/scripts/convert-with-core.py
+++ _template-folder/scripts/convert-with-core.py
@@ -3,4 +3,8 @@ import re
 from pathlib import Path
 
 #Transform from tab sep to comma sep
-
+#Update headings to snake_case
+#Transform data columns to meaningful data
+#Transform headings to meaningful data
+#Update data content to snake_case where controlled variables
+#Transpose data so variables are in columns and subjects in rows
