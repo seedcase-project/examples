9cd3c7da5a399425342e1654b33833be6a13b8b8
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2025-01-22 13:21:52 +0100

    feat: remove executable section from function

--- core/download.py
+++ core/download.py
@@ -19,15 +19,3 @@ def download_data(url, name):
         print(f"Error downloading the data: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
-if __name__ == "__main__":
-    # Set up command-line argument parsing
-    parser = argparse.ArgumentParser(description="Download data from a URL and save it to a file.")
-    parser.add_argument("url", type=str, help="The URL of the data to download.")
-    parser.add_argument("name", type=str, help="The name of the file to save the data as.")
-
-    # Parse the arguments
-    args = parser.parse_args()
-
-    # Call the download function
-    download_data(args.url, args.name)
--- male-seed-beetle/scripts/download-data.py
+++ male-seed-beetle/scripts/download-data.py
@@ -1,5 +1,7 @@
-from functions.data_download import download_data
-
+from core.download import download_data
+#from core import download_data
+#from examples.functions.download import download_data
+#from functions.download import download_data
 if __name__ == "__main__":
    # Provide the two arguments needed for download_data
     arg1 = "https://zenodo.org/records/4932381/files/BeetleMetabolicRate_Dryad.txt?download=1"  # Replace with your actual argument
