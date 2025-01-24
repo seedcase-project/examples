2ec85009be0ecb0e4b2cb8a0f20ae3aff2ac9971
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2025-01-24 15:14:42 +0100

    feat: write script for xlsx 2 csv for cortisol data

--- rhesus-monkeys/scripts/convert-with-core.py
+++ rhesus-monkeys/scripts/convert-with-core.py
@@ -24,7 +24,30 @@ from pathlib import Path
     # Check for snake-case
     # Remove "" and , and - from headings
     # Rename Metabolite to subject_ID
+
 # 06 - Cortisol_infant.blood.xlsx
     # Convert into csv
     # Clarify headings
     # Check for snake-case
+df = pl.read_excel("../data-raw/data6.xlsx")
+df.write_csv('../data-raw/data6.csv') # just for now so that I have a reference
+
+sample1 = '02:00'
+sample2 = '07:00'
+sample3 = '11:30'
+sample4 = '23:30'
+
+df1 = df.select(pl.col("Infant_ID"), pl.col("PD").alias("post_gestation_day"), pl.col("samp1").alias("cortisol_level"),)
+df2 = df.select(pl.col("Infant_ID"), pl.col("PD").alias("post_gestation_day"), pl.col("samp2").alias("cortisol_level"),)
+df3 = df.select(pl.col("Infant_ID"), pl.col("PD").alias("post_gestation_day"), pl.col("samp3").alias("cortisol_level"),)
+df4 = df.select(pl.col("Infant_ID"), pl.col("PD").alias("post_gestation_day"), pl.col("samp4").alias("cortisol_level"),)
+
+df_sample1 = df1.with_columns(pl.lit(sample1).alias("hours_into_sampling"))
+df_sample2 = df2.with_columns(pl.lit(sample2).alias("hours_into_sampling"))
+df_sample3 = df1.with_columns(pl.lit(sample3).alias("hours_into_sampling"))
+df_sample4 = df2.with_columns(pl.lit(sample4).alias("hours_into_sampling"))
+
+df_cortisol = pl.concat([df_sample1, df_sample2, df_sample3, df_sample4])
+
+#df_cortisol.glimpse()
+df_cortisol.write_csv('../data-raw/data6-ready.csv')
