ad9744d052527cf31120bc7ed177506664253f4e
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2025-01-29 12:55:53 +0100

    feat: add metadata to download script and adult metadata extract to convert-meta

--- rhesus-monkeys/scripts/convert-meta.py
+++ rhesus-monkeys/scripts/convert-meta.py
@@ -6,18 +6,41 @@ from pathlib import Path
 resource_dir = Path(__file__).resolve().parent.parent
 folder_path = resource_dir / "data-raw"
 
+# Create the infant metadata files
 df07 = pl.read_csv(folder_path / "data07.csv")
-
-df_infant1 = df07.select(pl.col("Infant_ID"), pl.col("Group").alias("obesity_classification_mother"), 
-pl.col("Mother_ID"), pl.col("GD_Delivery").alias("gestation_day_delivery"), pl.col("Mode_birth"), pl.col("Fostered"),
-pl.col("Foster_ID"), pl.col("Weight_PD7").alias("weight_at_x_days_old"), pl.col("ActualDay_PD7").alias("x_days_old"))
-
-df_infant1.glimpse()
-
 df08 = pl.read_csv(folder_path / "data08.csv")
 
-df_infant2 = df08.select(pl.col("Infant_ID"), pl.col("Group").alias("obesity_classification_mother"), 
-pl.col("Mother_ID"), pl.col("GD_Delivery").alias("gestation_day_delivery"), pl.col("Mode_birth"), pl.col("Fostered"),
-pl.col("Foster_ID"), pl.col("Infant_weight").alias("weight_at_x_days_old"), pl.col("PD").alias("x_days_old"))
+df_infant1 = df07.select(pl.col("Infant_ID"),  
+pl.col("Mother_ID"), pl.col("GD_Delivery").alias("gestation_day_delivery"), pl.col("Fostered"), pl.col("Foster_ID"))
+# infant gender is in 09 Maternal Blood
 
-df_infant2.glimpse()
+df_infant1.write_csv("../data-raw/data_infant.csv", separator=";")
+
+df_infant_w1 = df07.select(pl.col("Infant_ID"), pl.col("Weight_PD7").alias("weight_at_x_days_old"), pl.col("ActualDay_PD7").alias("x_days_old"))
+df_infant_w2 = df08.select(pl.col("Infant_ID"), pl.col("Infant_weight").alias("weight_at_x_days_old"), pl.col("PD").alias("x_days_old"))
+df_infant_weight = pl.concat([df_infant_w1, df_infant_w2])
+
+df_infant_weight.write_csv("../data-raw/data_infant_weight.csv", separator=";")
+
+# Create the adult metadata file
+df09 = pl.read_csv(folder_path / "data09.csv")
+df10 = pl.read_csv(folder_path / "data10.csv")
+
+df_adult09 = df09.select(pl.col("Mother_ID"), pl.col("Mother_age").alias("Age_at_conception"), pl.col("GD_Delivery").alias("gestation_day_at_delivery")
+, pl.col("Group").alias("obesity_classification"), pl.col("Mode_birth"), pl.col("Reject"), pl.col("BCS").alias("body_condition_score"))
+#, pl.col("Placenta_Width"), pl.col("Placenta_Height"), pl.col("Placenta_Thickness"), pl.col("EPV").alias("estimated_placenta_volume"))
+df_adult10 = df10.select(pl.col("Mother_ID"), pl.col("Mother_age").alias("Age_at_conception"), pl.col("GD_Delivery").alias("gestation_day_at_delivery")
+, pl.col("Group").alias("obesity_classification"), pl.col("Mode_birth"), pl.col("Reject"), pl.col("BCS").alias("body_condition_score"))
+# No placenta information in file 10, a couple of mothers not registered in file 9
+# No additional Mother_ID in file 11
+
+df_adult1 = pl.concat([df_adult09, df_adult10])
+
+df_adult1.unique() # doesn't do anything at present
+
+df_adult1.write_csv("../data-raw/data_adult.csv", separator=";")
+
+# 9 Meta maternal blood
+# Batch,GD_day,GD_targeted,Dilution_factor,Mother_Weight,Infant_sex
+# 10 Meta maternal urine
+# Batch,GD,Target_GD,Dilution_factor,Mother_Weight,Infant_sex
--- rhesus-monkeys/scripts/download-data.py
+++ rhesus-monkeys/scripts/download-data.py
@@ -60,3 +60,137 @@ if __name__ == "__main__":
     # Loop through the URLs and names and call the function for each pair
     for url, name in zip(urls, names):
         download_data(url, name)
+
+'''
+1 - Concentration_Infant.blood.csv
+* Columns: Samples. The same “Exp” represent the same sample in Metadata_Infant.blood.csv & Cytokine_Infant.blood.csv files.
+* Rows: Metabolites
+* The unit is uM. 
+
+2 - Concentration_Infant.urine.csv
+* Columns: Samples. The same “Exp” represent the same sample in Metadata_Infant.urine.csv file.
+* Rows: Metabolites
+* The unit is uM. 
+
+3 - Concentration_Maternal.blood.csv
+* Columns: Sample IDs. The same “Exp” represent the same sample in Concentration_Maternal.blood.csv, Cytokine_Infant.blood.csv, and Metadata_Maternal.blood.csv files. 
+* Rows: Metabolites
+* The unit is uM. 
+
+4 - Concentration_Maternal.placenta.csv
+* Columns: Samples. The same “Exp” represent the same sample in Metadata_Maternal.placenta.csv file.
+* Rows: Metabolites
+* The unit is uM.
+
+5 - Concentration_Maternal.urine.csv
+* Columns: Samples. The same “Exp” represent the same sample in Metadata_Maternal.urine.csv file.
+* Rows: Metabolites
+* The unit is uM. 
+
+6 - Cortisol_Infant.blood.csv
+* Variables 
+o Infant_ID: IDs of infants. 
+o Group: Lean or Obese.
+o PD: Exact postnatal day (PD) when samples were collected.
+o Mother_ID: IDs of mothers. 
+o Foster_ID: IDs of foster mothers. 
+o Infant_weight: Infant weights (kg) recorded at samples collection. 
+o samp1: Cortisol level from the 1st blood samples collected from infants at 11am, after being separated from mothers at 9am. 
+o samp2: Cortisol level from the 2nd blood samples collected 4 pm. After the blood collection, 500 ?g/kg dexamethasone was injected intramuscularly. 
+o samp3: Cortisol level from the 3rd blood samples collected at 8:30 am of the following day. After the blood collection, 2.5 IU of adrenocorticotropic hormone (ACTH) was injected intramuscularly.
+o samp4: Cortisol level from the 4th blood samples collected 30 min after ACTH injection. 
+o pctsuppression: Values obtained by dividing samp3 by samp2.
+* Missing data codes: Indicated by NAs.
+
+7 - Metadata_Infant.blood.csv
+* Variables 
+o Exp: Samples IDs. The same “Exp” represent the same sample in Concentration_Infant.blood.csv & Cytokine_Infant.blood.csv files.
+o Infant_ID: IDs of infants. 
+o Batch: The experiment was conducted over two batches (Batch1 or Batch2)
+o Group: Lean or Obese.
+o PD: Exact postnatal day (PD) when samples were collected.
+o Target_PD: Target PD for sample collection. 
+o Dilution_factor: Dilution factor used to prepare NMR samples. 
+o Mother_ID: IDs of mothers. 
+o GD_Delivery: Gestational day at delivery. 
+o Mode_birth: Mode of delivery.
+o Fostered: Yes (fostered) or No (was not fostered).
+o Foster_ID: IDs of foster mothers. 
+o Weight_PD7: Infant weights recorded at around PD7.
+o ActualDay_PD7: Actual day for “Weight_PD7”. 
+o Infant_weight: Infant weights (kg) recorded at samples collection. 
+* Missing data codes: Indicated by NAs.
+
+8 - Metadata_Infant.urine.csv
+* Variables 
+o Exp: Samples IDs. The same “Exp” represent the same sample in Concentration_Infant.urine.csv file.
+o Infant_ID: IDs of infants. 
+o Batch: The experiment was conducted over two batches (Batch1 or Batch2)
+o Group: Lean or Obese.
+o PD: Exact postnatal day (PD) when samples were collected.
+o Target_PD: Target PD for sample collection. 
+o Dilution_factor: Dilution factor used to prepare NMR samples. 
+o Mother_ID: IDs of mothers. 
+o GD_Delivery: Gestational day at delivery. 
+o Mode_birth: Mode of delivery.
+o Fostered: Yes (fostered) or No (was not fostered).
+o Foster_ID: IDs of foster mothers. 
+o Infant_weight: Infant weights (kg) recorded at samples collection. 
+* Missing data codes: Indicated by NAs.
+
+9 - Metadata_Maternal.blood.csv
+* Variables 
+o Exp: Samples IDs. The same “Exp” represent the same sample in Concentration_Maternal.blood.csv, Cytokine_Infant.blood.csv, and Metadata_Maternal.blood.csv files.
+o Mother_ID: IDs of mothers. 
+o Batch: The experiment was conducted over two batches (Batch1 or Batch2)
+o Group: Lean or Obese.
+o GD: Exact gestational day (GD) when samples were collected.
+o Target_GD: Target GD for sample collection. 
+o Dilution_factor: Dilution factor used to prepare NMR samples. 
+o BCS: Body Condition Score (BCS) 
+o Mother_Weight: Maternal weight in kg at sample collection. 
+o Mother_age: Maternal age at conception. 
+o GD_Delivery: Gestational day at delivery. 
+o Infant_sex: Infant sex.
+o Mode_birth: Mode of delivery.
+o Reject: Whether mothers rejected infants or not. 
+o Placenta_Width: Width of placenta at sample collection.
+o Placenta_Height: Height of placenta at sample collection.
+o Placenta_Thickness: Thickness of placenta at sample collection.
+o EPV: Estimated placental volume. 
+* Missing data codes: Indicated by NAs.
+
+10 - Metadata_Maternal.urine.csv
+* Variables 
+o Exp: Samples IDs. The same “Exp” represent the same sample in Concentration_Maternal.urine.csv file.
+o Mother_ID: IDs of mothers. 
+o Batch: The experiment was conducted over two batches (Batch1 or Batch2)
+o Group: Lean or Obese.
+o GD: Exact gestational day (GD) when samples were collected.
+o Target_GD: Target GD for sample collection. 
+o Dilution_factor: Dilution factor used to prepare NMR samples. 
+o BCS: Body Condition Score (BCS) 
+o Mother_Weight: Maternal weight in kg at sample collection. 
+o Mother_age: Maternal age at conception. 
+o GD_Delivery: Gestational day at delivery. 
+o Infant_sex: Infant sex.
+o Mode_birth: Mode of delivery.
+* Missing data codes: Indicated by NAs.
+
+11 - Metadata_Maternal.placenta.csv
+* Variables 
+o Exp: Samples IDs. The same “Exp” represent the same sample in Concentration_Maternal.placenta.csv file.
+o Mother_ID: IDs of mothers. 
+o Batch: The experiment was conducted over two batches (Batch1 or Batch2)
+o Group: Lean or Obese.
+o GD: Exact gestational day (GD) when samples were collected.
+o Target_GD: Target GD for sample collection. 
+o Dilution_factor: Dilution factor used to prepare NMR samples. 
+o BCS: Body Condition Score (BCS) 
+o Tissue_weight: Weight of placental tissue sample. 
+o V1: Volume of solvent used to extract (uL). Used to correct the metabolite concentration.
+o V2: Volume of polar layer (methanol + water) collected (uL). Used to correct the metabolite concentration.
+o V3: Buffer added to reconstitute the sample after freeze drying (uL). Used to correct the metabolite concentration. 
+* Missing data codes: Indicated by NAs.
+
+'''
