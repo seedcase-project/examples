8b84c946992f0800d1c1dd689c3279bb364922a4
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2025-02-13 14:33:16 +0100

    feat: revise convert-meat.py and add comment back to download-data

--- rhesus-monkeys/scripts/convert-meta.py
+++ rhesus-monkeys/scripts/convert-meta.py
@@ -1,85 +1,74 @@
 import polars as pl
 import re
 from pathlib import Path
+import janitor.polars
 
 # Set the folder path for raw-data
 resource_dir = Path(__file__).resolve().parent.parent
 folder_path = resource_dir / "data-raw"
 
 # Create the infant metadata files
-    # FIO There is one extra infant in 07, all infants in 08 are included in 07
-df07 = pl.read_csv(folder_path / "data07.csv")
-df08 = pl.read_csv(folder_path / "data08.csv")
+    # FIO All infants in meta_infant_ur are included in meta_infant_bl. There is one extra infant in meta_infant_bl.
+df_meta_infant_bl = pl.read_csv(folder_path / "metadata_infant_blood.csv") 
+df_meta_infant_ur = pl.read_csv(folder_path / "metadata_infant_urine.csv") 
 
-df_infant = df07.select(pl.col("Infant_ID"), pl.col("Mother_ID"), pl.col("GD_Delivery").alias("gestation_day_delivery")
-, pl.col("Fostered"), pl.col("Foster_ID"))
-# infant gender is in data_adult at present
-df_infant1 = df_infant.unique()
-df_infant1.write_csv("../data-raw/data_infant_meta.csv", separator=";")
+df_infant = df_meta_infant_bl.select(["Infant_ID", "Mother_ID", "GD_Delivery", "Fostered"
+, "Foster_ID"]).rename({"GD_Delivery": "gestation_day_delivery"}).unique().clean_names().write_csv(folder_path / "data_infant_meta.csv", separator=";")
+    # infant gender is in data_adult at present, all subjects are male, females are not included in the study
 
-df_infant_w1 = df07.select(pl.col("Infant_ID"), pl.col("Weight_PD7").alias("weight_at_x_days_old"), pl.col("ActualDay_PD7").alias("x_days_old"))
-df_infant_w2 = df08.select(pl.col("Infant_ID"), pl.col("Infant_weight").alias("weight_at_x_days_old"), pl.col("PD").alias("x_days_old"))
-df_infant_weight = pl.concat([df_infant_w1, df_infant_w2])
-df_infant_weight1 = df_infant_weight.unique()
-df_infant_weight1.write_csv("../data-raw/data_infant_weight.csv", separator=";")
+df_infant_w1 = df_meta_infant_bl.select(["Infant_ID", "Weight_PD7", "ActualDay_PD7"]).rename({"Weight_PD7": "weight_at_x_days_old", "ActualDay_PD7": "x_days_old"}).clean_names()
+df_infant_w2 = df_meta_infant_ur.select(["Infant_ID","Infant_weight","PD"]).rename({"Infant_weight": "weight_at_x_days_old", "PD": "x_days_old"}).clean_names()
+df_infant_weight = pl.concat([df_infant_w1, df_infant_w2]).unique().write_csv(folder_path / "data_infant_weight.csv", separator=";") # Concatenate (long)
 
-# Create the adult metadata files
-    # FIO There are no additional Mother_ID in file 11
-df09 = pl.read_csv(folder_path / "data09.csv")
-df10 = pl.read_csv(folder_path / "data10.csv")
-df11 = pl.read_csv(folder_path / "data11.csv")
-
-df_adult09 = df09.select(pl.col("Mother_ID"), pl.col("Mother_age").alias("Age_at_conception"), pl.col("GD_Delivery").alias("gestation_day_at_delivery")
-, pl.col("Group").alias("obesity_classification"), pl.col("Mode_birth"), pl.col("Reject"), pl.col("Infant_sex"))
-df_adult10 = df10.select(pl.col("Mother_ID"), pl.col("Mother_age").alias("Age_at_conception"), pl.col("GD_Delivery").alias("gestation_day_at_delivery")
-, pl.col("Group").alias("obesity_classification"), pl.col("Mode_birth"), pl.col("Reject"), pl.col("Infant_sex"))
-
-df_adult_w1 = df09.select(pl.col("Mother_ID"), pl.col("GD_day").alias("sample_gestation_day"), pl.col("GD_targeted").alias("target_gestation_day")
-, pl.col("Mother_Weight").alias("weight_at_gestation_day"), pl.col("BCS").alias("body_condition_score")) 
-df_adult_w2 = df10.select(pl.col("Mother_ID"), pl.col("GD").alias("sample_gestation_day"), pl.col("Target_GD").alias("target_gestation_day")
-, pl.col("Mother_Weight").alias("weight_at_gestation_day"), pl.col("BCS").alias("body_condition_score")) 
-
-# Concatenate and de-dup adult files
-df_adult1 = pl.concat([df_adult09, df_adult10])
-df_adult2 = df_adult1.unique()
-df_adult2.write_csv("../data-raw/data_adult_meta.csv", separator=";")
-
-df_adultw1 = pl.concat([df_adult_w1, df_adult_w2])
-df_adultw2 = df_adultw1.unique()
-df_adultw2.write_csv("../data-raw/data_adult_weight.csv", separator=";")
-
-# Create the linking table
-
-df_link07 = df07.select(pl.col("Infant_ID"), pl.col("Exp"), pl.col("PD").alias("day_sample_taken"), pl.col("Batch"))
-df_link08 = df08.select(pl.col("Infant_ID"), pl.col("Exp"), pl.col("PD").alias("day_sample_taken"), pl.col("Batch"))
-
-df_link09 = df09.select(pl.col("Mother_ID"), pl.col("Exp"), pl.col("GD_day").alias("day_sample_taken"), pl.col("GD_targeted").alias("target_sampling_day")
-, pl.col("Batch"), pl.col("Dilution_factor"))
-df_link10 = df10.select(pl.col("Mother_ID"), pl.col("Exp"), pl.col("GD").alias("day_sample_taken"), pl.col("Target_GD").alias("target_sampling_day")
-, pl.col("Batch"), pl.col("Dilution_factor"))
-df_link11 = df11.select(pl.col("Mother_ID"), pl.col("Exp"), pl.col("GD").alias("day_sample_taken"), pl.col("Target_GD").alias("target_sampling_day")
-, pl.col("Batch"), pl.col("Dilution_factor"))
+# Create the infant linking tables
 
 matter1 = "Blood"
 matter2 = "Urine"
+
+df_link_infant1 = df_meta_infant_bl.select(["Infant_ID", "Exp", "PD", "Batch"]).rename({"PD": "day_sample_taken"}).clean_names().with_columns(pl.lit(matter1).alias("type_of_matter"))
+df_link_infant2 = df_meta_infant_ur.select(["Infant_ID", "Exp", "PD", "Batch"]).rename({"PD": "day_sample_taken"}).clean_names().with_columns(pl.lit(matter2).alias("type_of_matter"))
+
+df_infant_link = pl.concat([df_link_infant1, df_link_infant2]).write_csv(folder_path / "data_infant_sample_meta.csv", separator=";") # Concatenate long
+
+# Create the adult metadata files
+    # FIO There are no additional Mother_ID in file 11
+df_metadata_maternal_bl = pl.read_csv(folder_path / "metadata_maternal_blood.csv")
+df_metadata_maternal_ur = pl.read_csv(folder_path / "metadata_maternal_urine.csv")
+df_metadata_maternal_pl = pl.read_csv(folder_path / "metadata_maternal_placenta.csv")
+
+df_adult_meta1 = df_metadata_maternal_bl.select(["Mother_ID", "Mother_age", "GD_Delivery", "Group", "Mode_birth", "Reject", "Infant_sex"]).rename({"Mother_age": "Age_at_conception"
+, "GD_Delivery": "gestation_day_at_delivery", "Group": "obesity_classification", "Mode_birth": "mode_of_birth"}).clean_names()
+df_adult_meta2 = df_metadata_maternal_ur.select(["Mother_ID", "Mother_age", "GD_Delivery", "Group", "Mode_birth", "Reject", "Infant_sex"]).rename({"Mother_age": "Age_at_conception"
+, "GD_Delivery": "gestation_day_at_delivery", "Group": "obesity_classification", "Mode_birth": "mode_of_birth"}).clean_names()
+df_adult = pl.concat([df_adult_meta1, df_adult_meta2]).unique().write_csv(folder_path / "data_adult_meta.csv", separator=";") # Concatenate long
+
+df_adult_weight1 = df_metadata_maternal_bl.select(["Mother_ID", "GD_day", "GD_targeted", "Mother_Weight", "BCS"]).rename({"GD_day": "sample_gestation_day"
+, "GD_targeted": "target_gestation_day", "Mother_Weight": "weight_at_gestation_day", "BCS": "body_condition_score"})
+df_adult_weight2 = df_metadata_maternal_ur.select(["Mother_ID", "GD", "Target_GD", "Mother_Weight", "BCS"]).rename({"GD": "sample_gestation_day"
+, "Target_GD": "target_gestation_day", "Mother_Weight": "weight_at_gestation_day", "BCS": "body_condition_score"})
+df_adult_weight = pl.concat([df_adult_weight1, df_adult_weight2]).unique().write_csv(folder_path / "data_adult_weight.csv", separator=";") # Concatenate long
+
+# Create the adult linking tables
+
+# matter1 = "Blood" define above in infant linking tables
+# matter2 = "Urine" define above in infant linking tables
 matter3 = "Placenta"
 
-df_lnk1 = df_link07.with_columns(pl.lit(matter1).alias("type_of_matter"))
-df_lnk2 = df_link08.with_columns(pl.lit(matter2).alias("type_of_matter"))
-df_lnk3 = df_link09.with_columns(pl.lit(matter1).alias("type_of_matter"))
-df_lnk4 = df_link10.with_columns(pl.lit(matter2).alias("type_of_matter"))
-df_lnk5 = df_link11.with_columns(pl.lit(matter3).alias("type_of_matter"))
+df_link_adult1 = df_metadata_maternal_bl.select(["Mother_ID", "Exp", "GD_day", "GD_targeted", "Batch", "Dilution_factor"]).rename({"GD_day": "day_sample_taken"
+, "GD_targeted": "target_sampling_day"}).with_columns(pl.lit(matter1).alias("type_of_matter"))
+df_link_adult2 = df_metadata_maternal_ur.select(["Mother_ID", "Exp", "GD", "Target_GD", "Batch", "Dilution_factor"]).rename({"GD": "day_sample_taken"
+, "Target_GD": "target_sampling_day"}).with_columns(pl.lit(matter2).alias("type_of_matter"))
+df_link_adult3 = df_metadata_maternal_pl.select(["Mother_ID", "Exp", "GD", "Target_GD", "Batch", "Dilution_factor"]).rename({"GD": "day_sample_taken"
+, "Target_GD": "target_sampling_day"}).with_columns(pl.lit(matter3).alias("type_of_matter"))
 
-df_infant_link = pl.concat([df_lnk1, df_lnk2])
-df_infant_link.write_csv('../data-raw/data_infant_sample_meta.csv')
-df_adult_link = pl.concat([df_lnk3, df_lnk4, df_lnk5])
-df_adult_link.write_csv('../data-raw/data_adult_sample_meta.csv')
+df_adult_link = pl.concat([df_link_adult1, df_link_adult2, df_link_adult3]).write_csv(folder_path / "data_adult_sample_meta.csv", separator=";") # Concatenate long
+
 
 # Create the placenta file
-# Both from 09 (Placenta_Width,Placenta_Height,Placenta_Thickness,EPV) and from 11 (see below)
-# More than one measure in 09?
+# Both from metadata_maternal_bl (Placenta_Width,Placenta_Height,Placenta_Thickness,EPV) and from 11 (see below)
+# More than one measure in metadata_maternal_bl?
 '''
-11 - Metadata_Maternal.placenta.csv
+11 - Metadata_Maternal_placenta.csv
 * Variables 
 o Exp: Samples IDs. The same “Exp” represent the same sample in Concentration_Maternal.placenta.csv file.
 o Mother_ID: IDs of mothers. 
