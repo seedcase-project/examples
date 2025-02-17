c6e34cc58b4751eb999afb280c2c515a6a052fec
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2025-02-17 14:28:38 +0100

    feat: update package.py with contributors and source

--- male-seed-beetle/male_seed_beetle/male-seed-beetle/packages/1/README.md
+++ male-seed-beetle/male_seed_beetle/male-seed-beetle/packages/1/README.md
@@ -5,10 +5,10 @@ N/A
 | Field    | Value                                   |
 |----------|-----------------------------------------|
 | Name     | N/A     |
-| ID       | `38074db8-24ce-48d7-8925-9bc2facc14b3`       |
+| ID       | `f5b756d3-d6e0-4287-b362-d0726f656b6d`       |
 | Version  | `0.1.0`  |
 | Homepage | N/A |
-| Created  | 12 February 2025, 14:08  |
+| Created  | 17 February 2025, 14:19  |
 | Licenses | N/A  |
 
 No resources available.
--- male-seed-beetle/male_seed_beetle/package.py
+++ male-seed-beetle/male_seed_beetle/package.py
@@ -14,40 +14,43 @@ properties = sp.PackageProperties(
     contributors=[
         sp.ContributorProperties(
             title="Elina Immonen",
-            email="",
             path="https://www.uu.se/en/contact-and-organisation/staff?query=N12-1639",
             roles=["creator"],
-        )
+        ),
         sp.ContributorProperties(
             title="Johanna Liljestrand-Rönn",
-            email="",
             path="https://www.uu.se/en/contact-and-organisation/staff?query=N2-1345",
             roles=["creator"],
-        )
+        ),
         sp.ContributorProperties(
             title="Christopher Watson",
-            email="",
             path="https://www.uu.se/en/department/ecology-and-genetics/research/evolutionary-biology/immonen-lab",
             roles=["creator"],
-        )
+        ),
         sp.ContributorProperties(
             title="David Berger",
-            email="",
             path="https://www.uu.se/en/contact-and-organisation/staff?query=N11-2446",
             roles=["creator"],
-        )
+        ),
         sp.ContributorProperties(
             title="Göran Arnqvist",
-            email="",
             path="https://www.uu.se/en/contact-and-organisation/staff?query=N1-1284",
             roles=["creator"],
+        ),
+    ],
+    sources=[
+        sp.SourceProperties(
+            title=(
+                "Complex mitonuclear interactions and metabolic costs "
+                "of mating in male seed beetles"
+            ),
+            path="https://zenodo.org/record/4932381",
         )
     ],
     licenses=[
         sp.LicenseProperties(
             name="CCO_1.0",
             path="https://creativecommons.org/publicdomain/zero/1.0/legalcode",
-            source="https://zenodo.org/record/4932381",
             title="CCO 1.0 UNIVERSAL",
         )
     ],
